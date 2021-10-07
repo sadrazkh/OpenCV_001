5419a1491a4561d4fdb2178a5fe1e9cca1a10a46
sadra zadeh khameneh <sadra.z.kh@gmail.com>
2021-10-07 16:28:35 +0330

    Write Video

--- Read_Write_Video.py
+++ Read_Write_Video.py
@@ -3,14 +3,21 @@ import cv2
 cap = cv2.VideoCapture("Video/test001.mp4")
 # cap = cv2.VideoCapture(0)
 # while cap.isOpened()
+fourcc = cv2.VideoWriter_fourcc('M', 'P', '4', 'V')
+h = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+w = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+
+out = cv2.VideoWriter('TestWrite.mp4', fourcc, 20.0, (1920,1080))
+
 while True:
     ret, frame = cap.read()
     if not ret:
         break
+    out.write(frame)
     cv2.imshow('Test Video', frame)
-
     if cv2.waitKey(1) & 0xFF == ord('q') or cv2.waitKey(1) & 0xFF == ord('Q'):
         break
 
 cap.release()
+out.release()
 cv2.destroyAllWindows()
